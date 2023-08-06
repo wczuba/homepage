a16d8eba1d344c9b3c6e4a2469c9e0759fea3b90
Wojciech Czuba <wojtek.czuba@gmail.com>
2023-08-06 10:46:37 +0200

    Add first DOM manipulation and BEM

--- main.js
+++ main.js
@@ -3,3 +3,9 @@ const age = 38
 
 console.log(`Nazywam się ${firstName} i mam ${age} lat`)
 alert(`Nazywam się ${firstName} i mam ${age} lat`)
+
+const header = document.querySelector('.page-header__heading--js')
+
+console.log(header.innerHTML)
+
+header.innerHTML = 'Hej!'
