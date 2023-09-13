bfffbc82e26179809235bc1f8792fc19812c331d
Wojciech Czuba <wojtek.czuba@gmail.com>
2023-09-13 19:14:33 +0200

    exercises

--- main.js
+++ main.js
@@ -9,3 +9,24 @@ const header = document.querySelector('.page-header__heading--js')
 console.log(header.innerHTML)
 
 header.innerHTML = 'Hej!'
+
+function calculate(MyNumber) {
+    return MyNumber*2
+}
+
+console.log(calculate(3))
+
+const deathStar = {
+    diameter: 120000, // property
+    fire: (target) => { // method
+        console.log(`${target} destroyed!`)
+    },
+    isOperating: true,
+    levels: 357,
+    name: 'Death Star',
+    population: 10000,
+}
+
+console.log(deathStar.name)
+
+deathStar.fire('Arka Gdynia')
